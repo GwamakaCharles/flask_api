ebe7470bcee820077ccec5b77e61208d2228958f
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-07 19:14:12 +0300

    added 404 and paginate

--- README.md
+++ README.md
@@ -9,17 +9,24 @@ The API exposes endpoints to fetch users from the database :
 The 'users' table has column 'name'
 
 ### 10,000 DUMMY
+
 insert into users (
-	name)
+name)
 select
-	left(md5(i::text), 10)
+left(md5(i::text), 10)
 from generate_series(1,10000) s(i);
 
 #### Endpoints
 
 READ:
 
+<!-- Retrieve a list of users -->
+
 GET users list - {{url}}/users
+
+<!-- Retrieve a User by name -->
+
+GET user by name - {{url}}/user/<name>
 
 url :
 "https://quiet-anchorage-71805.herokuapp.com/"
--- resources/user.py
+++ resources/user.py
@@ -4,12 +4,14 @@ from models.user import UserModel
 class Users(Resource):
     
     def get(self):
-        return {'users' : [user.json() for user in UserModel.query.all()]}
+        return {'users' : [user.json() for user in UserModel.query.all().paginate(1,20)]}
         
 class User(Resource):
 
     def get(self,name):
-        user = UserModel.find_user_by_name(name) # call the class function
-        if user: 
-            return user.json() # if the User exists return the User 
-        return {'message': 'User not found'}, 404  # else return User not found 
+        user = UserModel.find_user_by_name(name).get_or_404()
+        return user.json()
+        # call the class function
+        # if user: 
+        #     return user.json() # if the User exists return the User 
+        # return {'message': 'User not found'}, 404  # else return User not found 
