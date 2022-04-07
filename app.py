43210b8c4116206f9ef9cc630e2fb267729f13aa
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-07 21:08:43 +0300

    fixed readme

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from resources.user import User, Users
 
 from db import db
 app = Flask(__name__)
-# app.config.from_object(BaseConfig)
 api = Api(app)
 
 uri = os.getenv("DATABASE_URL") 
@@ -25,13 +24,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def create_tables():
     db.create_all()
 
-# api.add_resource(Users, '/users')
 
-@api.resource(Users, '/users')
-class Foo(Resource):
-    def get(self):
-        return 'Hello, World!'
-        
+api.add_resource(Users, '/users')
 api.add_resource(User, '/user/<string:name>')
 
 
