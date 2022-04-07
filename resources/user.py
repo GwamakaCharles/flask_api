d38bafe28ae31f08dab1ae3ec982f5a2e5dab140
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-07 19:49:45 +0300

    retrieve page and per_page items from request query

--- resources/user.py
+++ resources/user.py
@@ -4,14 +4,10 @@ from models.user import UserModel
 class Users(Resource):
     
     def get(self):
-        return {'users' : [user.json() for user in UserModel.query.paginate(1,20).items]}
+        return {'users' : [user.json() for user in UserModel.query.paginate().items]}
         
 class User(Resource):
 
     def get(self,name):
         user = UserModel.find_user_by_name(name)
         return user.json()
-        # # call the class function
-        # if user: 
-        #     return user.json() # if the User exists return the User 
-        # return {'message': 'User not found'}, 404  # else return User not found 
