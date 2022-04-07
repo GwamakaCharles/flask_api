2588911d7406ee4c62bd83f91c37147ecb38d326
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-04-07 18:49:38 +0300

    fixed _User to User

--- resources/user.py
+++ resources/user.py
@@ -1,15 +1,15 @@
-from flask_restful import Resource, reqparse
-from models.user import User
+from flask_restful import Resource
+from models.user import UserModel
 
 class Users(Resource):
     
     def get(self):
-        return {'users' : [user.json() for user in User.query.all()]}
+        return {'users' : [user.json() for user in UserModel.query.all()]}
         
-class _User(Resource):
+class User(Resource):
 
     def get(self,name):
-        user = User.find_user_by_name(name) # call the class function
+        user = UserModel.find_user_by_name(name) # call the class function
         if user: 
             return user.json() # if the User exists return the User 
         return {'message': 'User not found'}, 404  # else return User not found 
